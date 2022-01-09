342bda80c494b24d859f060132d63c6c193c4240
rxd5mp0 <robert_deckard@homedepot.com>
2022-01-09 15:40:30 -0600

    adds image optimization through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,28 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name(file) {
+                return "[path][name].[ext]";
+              },
+              publicPath: function (url) {
+                return url.replace("../", "/assets");
+              },
+            },
+          },
+          { loader: "image-webpack-loader" },
+        ],
+      },
+    ],
+  },
   mode: "development",
   plugins: [
     new webpack.ProvidePlugin({
